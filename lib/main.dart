e640928e0c5f718f7f1324948c0f33b641ad4bf8
priyanshi-devOps11 <srivastavapriyanshi8081@gmail.com>
2025-10-02 21:55:18 +0530

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'app.dart';
 import 'providers/auth_provider.dart';
+import 'services/api_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Initialize secure storage
+  const secureStorage = FlutterSecureStorage();
+
+  // Initialize API service
+  final apiService = ApiService();
+
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
-      child: MyApp(),
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(secureStorage, apiService),
+        ),
+      ],
+      child: const GetCalleyApp(),
     ),
   );
 }

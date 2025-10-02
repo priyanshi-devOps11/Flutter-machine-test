250570618a2dbf05b136edb1b2772de963eea2fa
priyanshi-devOps11 <srivastavapriyanshi8081@gmail.com>
2025-10-02 22:10:11 +0530

    updated user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,36 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id']?.toString() ?? '',
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phone: json['phone'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $id, name: $name, email: $email, phone: $phone)';
+  }
+}

e2a1fb11d6c1c8336343e6ee505468af65f70561
priyanshi-devOps11 <srivastavapriyanshi8081@gmail.com>
2025-10-02 23:17:34 +0530

    updated MainActivity.java

--- android/app/src/main/java/com/example/get_calley_test/MainActivity.java
+++ android/app/src/main/java/com/example/get_calley_test/MainActivity.java
@@ -4,3 +4,36 @@ import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 }
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.yourname.getcalley.R;
+import com.yourname.getcalley.utils.PreferencesManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            PreferencesManager prefsManager = PreferencesManager.getInstance(this);
+
+            Intent intent;
+            if (prefsManager.isLoggedIn()) {
+                intent = new Intent(MainActivity.this, DashboardActivity.class);
+            } else {
+                intent = new Intent(MainActivity.this, LanguageSelectActivity.class);
+            }
+
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}

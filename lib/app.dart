c2bd993b2ca1cdf3347e96eaff86459eaf8427dc
priyanshi-devOps11 <srivastavapriyanshi8081@gmail.com>
2025-10-04 14:38:06 +0530

    updated app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart';
 import 'screens/splash.dart';
 import 'screens/language_select.dart';
 import 'screens/register.dart';
 import 'screens/otp_verify.dart';
+import 'screens/login.dart';
 import 'screens/dashboard.dart';
 
 class GetCalleyApp extends StatelessWidget {
@@ -16,35 +15,12 @@ class GetCalleyApp extends StatelessWidget {
       title: 'Get Calley',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: const Color(0xFF1E88E5),
+        primaryColor: const Color(0xFF007AFF),
         scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF1E88E5),
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.white),
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF1E88E5),
-            foregroundColor: Colors.white,
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        fontFamily: 'Roboto',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF007AFF),
+          primary: const Color(0xFF007AFF),
         ),
       ),
       home: const SplashScreen(),
@@ -52,6 +28,7 @@ class GetCalleyApp extends StatelessWidget {
         '/language': (context) => const LanguageSelectScreen(),
         '/register': (context) => const RegisterScreen(),
         '/otp': (context) => const OtpVerifyScreen(),
+        '/login': (context) => const LoginScreen(),
         '/dashboard': (context) => const DashboardScreen(),
       },
     );
